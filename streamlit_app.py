2a8bda4b5af46c231eee0aa70bbb873a33d75903
Oma Antonio Mora Gonzalez <44041160+DevOMG23@users.noreply.github.com>
2024-04-11 16:44:23 -0600

    Update streamlit_app.py
    dataframe added

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,3 +61,4 @@ if ingredients_list:
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
