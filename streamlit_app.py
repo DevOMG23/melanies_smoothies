92bee73365082a38988a27e0c24774722a38b100
Oma Antonio Mora Gonzalez <44041160+DevOMG23@users.noreply.github.com>
2024-04-11 16:41:56 -0600

    Update streamlit_app.py
    json added.

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,4 +60,4 @@ if ingredients_list:
 # New section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
