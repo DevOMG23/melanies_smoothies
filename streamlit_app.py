2c3d94392a6d25662323d087c982e4781ad08a88
Oma Antonio Mora Gonzalez <44041160+DevOMG23@users.noreply.github.com>
2024-04-11 16:51:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col import requests
+from snowflake.snowpark.functions import col 
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
