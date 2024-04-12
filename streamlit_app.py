21c6ef7bb216a4e0c5b6828daace20d948135475
Oma Antonio Mora Gonzalez <44041160+DevOMG23@users.noreply.github.com>
2024-04-12 17:53:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ pd_df=my_dataframe.to_pandas()
 # st.stop()
 
 ingredients_list = st.multiselect(
-    'Choose up to 5 iingredients:'
+    'Choose up to 5 ingredients:'
     , my_dataframe
     , max_selections = 5
 )
